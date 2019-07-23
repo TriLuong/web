bc03a287b58c6a5e99625f63a9be02ad97de0242
Tri Luong <lxt0094@gmail.com>
2019-07-23 23:08:31 +0700

    Layout for Header of ManageUser page.
    Signed-off-by: Tri Luong <lxt0094@gmail.com>

---
+++ src/containers/ManageUser/index.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import Header from '../../components/Header';
+
+class ManageUser extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default ManageUser;
