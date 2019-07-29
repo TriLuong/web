067afe432dd81d0539dadd960054c21432094125
Hoa Vo <vovanhoait@gmail.com>
2019-07-29 11:06:19 +0700

    fix empty user when select

--- src/pages/App/selectors.js
+++ src/pages/App/selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect';
 const selectRootReducer = state => state.get('rootReducer');
 export const makeGetUser = () => createSelector(
   selectRootReducer,
-  rootReducer => rootReducer.get('user').toJS(),
+  rootReducer => (rootReducer.get('user').toJS ? rootReducer.get('user').toJS() : null),
 );
 
 export default {};
