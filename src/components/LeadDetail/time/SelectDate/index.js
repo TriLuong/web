5b3a8a027974bab228b6caf579c5f7f1b4e98b27
Hoa Vo <vovanhoait@gmail.com>
2019-08-14 01:59:59 +0700

    add select date

--- src/components/LeadDetail/time/SelectDate/index.js
+++ src/components/LeadDetail/time/SelectDate/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import Calendar from 'components/LeadDetail/time/Calendar';
+import { SelectField } from 'components/common/form';
 import moment from 'moment';
 import './style.scss';
 
@@ -7,13 +8,69 @@ type Props = {
   onDateChange: () => {},
   initialDate: '',
 };
+
+const YEARS = (() => {
+  const years = [];
+  for (let i = moment().year(); i <= moment().year() + 2; i++) {
+    years.push({ value: i, label: i });
+  }
+  return years;
+})();
+const MONTHS = (() => moment.months().map((label, value) => ({ value, label })))();
+
 class SelectDate extends PureComponent<Props> {
+  state = { monthObj: null };
+
+  renderMonthElement = monthObj => {
+    this.setState({ monthObj });
+  };
+
+  onChangeMonth = ({ value }) => {
+    const { monthObj } = this.state;
+    const { month, onMonthSelect } = monthObj || {};
+    onMonthSelect(month, value);
+  };
+
+  onChangeYear = ({ value }) => {
+    const { monthObj } = this.state;
+    const { month, onYearSelect } = monthObj || {};
+    onYearSelect(month, value);
+  };
+
+  renderMonth = () => {
+    const { monthObj } = this.state;
+    const { month } = monthObj || {};
+    const valueMonth = MONTHS.find(item => item.value === month.month());
+    const valueYear = YEARS.find(item => item.value === month.year());
+    return (
+      <div style={{ display: 'flex' }}>
+        <div>
+          <SelectField
+            className="select-month"
+            options={MONTHS}
+            value={valueMonth}
+            onChange={this.onChangeMonth}
+          />
+        </div>
+        <div>
+          <SelectField
+            className="select-year"
+            options={YEARS}
+            value={valueYear}
+            onChange={this.onChangeYear}
+          />
+        </div>
+      </div>
+    );
+  };
+
   render() {
     const { onDateChange, initialDate } = this.props;
     return (
-      <div>
-        <div className="titleCalendar">
+      <div className="wrapper-calendar">
+        <div className="titleCalendar" style={{ display: 'flex', justifyContent: 'space-between' }}>
           <p>Select Date</p>
+          {this.state.monthObj && this.renderMonth()}
         </div>
         <hr />
         <Calendar
@@ -22,6 +79,7 @@ class SelectDate extends PureComponent<Props> {
           onDateChange={onDateChange}
           daySize={50}
           initialDate={initialDate ? moment(initialDate) : ''}
+          renderMonthElement={this.renderMonthElement}
         />
       </div>
     );
--- src/components/common/form/Select.js
+++ src/components/common/form/Select.js
@@ -2,41 +2,20 @@ import React, { PureComponent } from 'react';
 import Select from 'react-select';
 
 type Props = {
-  onChange: () => {},
-  value: String,
-  name: string,
-  defaultValue: string,
-  options: [],
-  placeholder: String,
+  className: String,
 };
 class SelectField extends PureComponent<Props> {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = selectedOption => {
-    const { onChange } = this.props;
-    onChange(selectedOption);
-  };
-
   render() {
-    const { className, value, name, defaultValue, options, placeholder, ...rest } = this.props;
-
+    const { className, ...rest } = this.props;
     return (
       <div className={`wrap-select ${className}`}>
         <Select
           {...rest}
-          name={name}
-          defaultValue={defaultValue}
-          value={value}
-          onChange={this.handleChange}
-          options={options}
           clearable={false}
           className="wrap-select__inner"
-          placeholder={placeholder}
           classNamePrefix="wrap-select"
           isSearchable={false}
+          navPrev={null}
         />
       </div>
     );
